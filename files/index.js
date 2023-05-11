07aa5200c6f312cfff0abd25867e31b976747367
SrijanTimsina <srijan.timsina.39@gmail.com>
2023-05-12 00:10:59 +0545

    first commit

---
+++ files/index.js
@@ -0,0 +1,59 @@
+const now = new Date();
+const dateTime = document.getElementById("date-time");
+
+const minDateTimeISO = now.toISOString().slice(0, 16);
+
+const utcHour = now.getUTCHours();
+const utcMinute = now.getUTCMinutes();
+
+document.getElementById(
+	"current-time-display"
+).innerText = `${utcHour}:${
+	utcMinute > 9 ? utcMinute : "0" + utcMinute
+} ${utcHour > 11 ? "P.M." : "A.M."}`;
+
+dateTime.min = minDateTimeISO;
+
+document.getElementById("copy-text").addEventListener("click", () => {
+	navigator.clipboard.writeText(
+		document.getElementById("text-copy").innerText
+	);
+	document.getElementById("copy").style.display = "none";
+	document.getElementById("copied").style.display = "block";
+});
+
+const submitForm = () => {
+	const status = true;
+
+	// Form Submission Code here //
+	// If form submission failed change status to false //
+
+	document.getElementById("request-form-container").style.display =
+		"none";
+	if (status) {
+		document.getElementById("submission-success").style.display =
+			"block";
+	} else {
+		document.getElementById("submission-failed").style.display =
+			"block";
+	}
+};
+
+document
+	.getElementById("form-request")
+	.addEventListener("submit", (e) => {
+		e.preventDefault();
+		const time = new Date();
+		if (dateTime.value < time.toISOString().slice(0, 16)) {
+			alert("Please Enter a valid date");
+			return;
+		}
+		const txnHash = document.getElementById("txn-hash").value;
+		if (document.getElementById("txn-hash").value == "") {
+			alert("Please Enter the payment transaction hash");
+			return;
+		}
+
+		submitForm();
+		return;
+	});
